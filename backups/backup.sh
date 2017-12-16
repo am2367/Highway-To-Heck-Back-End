e4ac3287b3d12c83ab0c21d58ee68e24173528ed
Eric Koshykar <ek225@njit.edu>
2017-12-15 21:23:59 -0500

    Fixed minor bug in backup.sh

--- backups/backup.sh
+++ backups/backup.sh
@@ -7,20 +7,19 @@ host="127.0.0.1"
 db_name="login"
 
 # Other options
-backup_path="/home/ekoshykar/git/it490f17/backups"
+backup_path="/home/prod/git/it490f17/backups"
 date=$(date +" %m %d %Y")
 
 #set default file permissions
 umask 177
 
 #dump database into sql file
-#dump = "--user=$user --password=$password --host=$host $db_name > $backup_path/$db_name-$date.sql"
-echo $date
-mysqldump -u 'root' -p'it490' login > /home/ekoshykar/git/it490f17/backups/"login-$date.sql"
+
+
+mysqldump --user=$user --password=$password $db_name> $backup_path/"login-$date.sql"
 
 
 
-#mysqldump --user=$user --password=$password --host=$host $db_name > $backup_path/$db_name-$date.sql
 
 #delete files older than 30 days
 find $backup_path/* -mtime +30 -exec rm {} \;
